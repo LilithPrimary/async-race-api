9c48ff51deb467621814a1ed757ef16d580fb73f
Denna <lilithprimary@gmail.com>
2022-09-16 11:41:18 +0200

    feat: upd json-server

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
 const jsonServer = require('json-server');
-
-const db = {
-    garage: [
-        {
-            "name": "Tesla",
-            "color": "#e6e6fa",
-            "id": 1,
-        },
-        {
-            "name": "BMW",
-            "color": "#fede00",
-            "id": 2,
-        },
-        {
-            "name": "Mersedes",
-            "color": "#6c779f",
-            "id": 3,
-        },
-        {
-            "name": "Ford",
-            "color": "#ef3c40",
-            "id": 4,
-        },
-    ],
-    winners: [
-        {
-            id: 1,
-            wins: 1,
-            time: 10,
-        }
-    ]
-};
+const db = require('./db.json');
 
 const server = jsonServer.create();
-const router = jsonServer.router(db);
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const state = { velocity: {}, blocked: {} };
+const state = {velocity: {}, blocked: {}};
 
 server.use(middlewares);
 
 server.patch('/engine', (req, res) => {
-    const { id, status } = req.query;
+    const {id, status} = req.query;
 
     if (!id || !status || !/^(started)|(stopped)|(drive)$/.test(status)) {
         return res.status(400).send('Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"');
@@ -59,7 +28,7 @@ server.patch('/engine', (req, res) => {
 
         if (!velocity) return res.status(404).send('Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?');
         if (state.blocked[id]) return res.status(429).send('Drive already in progress. You can\'t run drive for the same car twice while it\'s not stopped.');
-        
+
         state.blocked[id] = true;
 
         const x = Math.round(distance / velocity);
@@ -74,7 +43,7 @@ server.patch('/engine', (req, res) => {
             setTimeout(() => {
                 delete state.velocity[id];
                 delete state.blocked[id];
-                res.header('Content-Type', 'application/json').status(200).send(JSON.stringify({ success: true }));
+                res.header('Content-Type', 'application/json').status(200).send(JSON.stringify({success: true}));
             }, x);
         }
     } else {
@@ -89,7 +58,7 @@ server.patch('/engine', (req, res) => {
             delete state.blocked[id];
         }
 
-        setTimeout(() => res.header('Content-Type', 'application/json').status(200).send(JSON.stringify({ velocity, distance })), x);
+        setTimeout(() => res.header('Content-Type', 'application/json').status(200).send(JSON.stringify({velocity, distance})), x);
     }
 });
 
